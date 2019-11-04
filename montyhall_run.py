45358b0d67567687179dadd35ee0da5212072dd2
Pj Meyer <pj.thehumanist@gmail.com>
2019-11-03 21:27:29 -0500

    Cleaned up a lot of the code, reduced complexity

--- montyhall_run.py
+++ montyhall_run.py
@@ -2,113 +2,66 @@ from __future__ import division
 import random
 
 print("\nLet's generate some Monty Hall games!")
-while True:
-	CHOOSE_TO_SWITCH = input("Do we always want to switch or stay: ")
-	if CHOOSE_TO_SWITCH == "switch":
-		CHOOSE_TO_SWITCH = True
-		break
-	elif CHOOSE_TO_SWITCH == "stay":
-		CHOOSE_TO_SWITCH = False
-		break
-	print("Not a valid entry, type 'switch' or 'stay' to continue.")
 
-while True:
-	iterations = input("How many random games should we generate: ")
-	try:
-		iterations = int(iterations)
-		break
-	except:
-		print("Not a valid number, try again")
+#ask for switch/stay, error check
+def ask_for_switch():
+	while True:
+		chose_to_switch = input("Do we always want to switch or stay: ")
+		if chose_to_switch == "switch":
+			chose_to_switch = True
+			return chose_to_switch
+		elif chose_to_switch == "stay":
+			chose_to_switch = False
+			return chose_to_switch
+		print("Not a valid entry, type 'switch' or 'stay' to continue.")
 
-#storing the outcomes here, 1 is a win, 0 is a loss
-outcomes = []
+#ask for how many iterations, error check
+def ask_for_iterations():	
+	while True:
+		iterations = input("How many random games should we generate: ")
+		try:
+			iterations = int(iterations)
+			return int(iterations)
+		except:
+			print("Not a valid number, try again")
 
-for i in range(0, iterations):
+#start the game
+def play_game(chose_to_switch):
+		print("-")
 
-	#set the random chosen door and the random win door
-	win_door = random.randint(1,3)
-	chosen_door = random.randint(1,3)
+		#set and print the random chosen door and set the random win door
+		win_door = random.randint(1,3)
+		chosen_door = random.randint(1,3)
+		print(f"Chose door number {chosen_door}")
 
-	did_we_win = False
+		#set and print the door we're exposing as the "decoy"
+		if win_door == chosen_door:
+			exposed_door = random.randint(1,3)
+			while exposed_door == chosen_door:
+				exposed_door = random.randint(1,3)
+		elif win_door != chosen_door:
+			exposed_door = 6 - win_door - chosen_door
+		print(f"Door number {exposed_door} revealed as bad")
 
-	if(CHOOSE_TO_SWITCH):
-		print("-----")
-		#create an array of doors
-		doors = ['bad','bad','bad']
+		#check for switch, then print action and door
+		if chose_to_switch:
+			chosen_door = 6 - chosen_door - exposed_door
+			print(f"Switched to door number {chosen_door}")
+		elif not chose_to_switch:
+			print(f"Stayed with door number {chosen_door}")
 
-		if(chosen_door == win_door):
-			doors[chosen_door - 1] = 'chosen_win'
-		else:
-			doors[(chosen_door - 1)] = 'chosen_bad'
-			doors[(win_door - 1)] = 'win'
+		print(f"Door number {win_door} was the right door, win: {chosen_door == win_door}")
 
-		print(doors)
+		return chosen_door == win_door
 
-		#if they don't match, just eliminate the non-chosen non-win door
-		if(win_door != chosen_door):
-			doors.remove('bad')
-		#person chose the winning door, removing one of the others
-		elif(win_door == chosen_door):
-			if(chosen_door == 1):
-				doors.remove(str(doors[random.randint(1,2)]))
-			elif(chosen_door == 2):
-				rand = random.randint(0,1)
-				if(rand == 1):
-					rand = 2
-				doors.remove(str(doors[rand]))
-			elif(chosen_door == 3):
-				doors.remove(str(doors[random.randint(0,1)]))
+#calulate and print the results
+def print_statistics(win_count, run_count):
+	print(f"\nTotal wins: {win_count} out of {run_count}")
+	print (f"We won {round(win_count / run_count * 100, 2)}% of the time.")
 
-		print("After elimination: " + str(doors))
-
-		#set the "new" doors (just make them doors 1 & 2 now)
-		new_chosen_door = -1
-		new_win_door = -2
-		if 'chosen_bad' in doors:
-			new_chosen_door = doors.index('chosen_bad')
-		elif 'chosen_win' in doors:
-			new_chosen_door = doors.index('chosen_win')
-
-		if 'win' in doors:
-			new_win_door = doors.index('win')
-		elif 'bad' in doors:
-			new_win_door = doors.index('chosen_win')
-
-		#switch doors
-		if(new_chosen_door == 0):
-			new_chosen_door = 1
-		elif(new_chosen_door == 1):
-			new_chosen_door = 0
-
-		print("Switched doors. Chosen door: " + str(new_chosen_door) + " Win door: " + str(new_win_door))
-
-		if(new_win_door == new_chosen_door):
-			outcomes.append(1)
-			did_we_win = True
-		else:
-			outcomes.append(0)
-			did_we_win = False
-
-	elif(not(CHOOSE_TO_SWITCH)):
-		if(win_door == chosen_door):
-			outcomes.append(1)
-			did_we_win = True
-		else:
-			outcomes.append(0)
-			did_we_win = False
-
-	print("Results: Winning door: " + str(win_door) +
-	" | Chosen door: " + str(chosen_door) +
-	" | Win: " + str(did_we_win))
-
-#print(str(outcomes[0]))
-
-total = 0
-for x in range(0, iterations):
-	#print("outcomes value: " + str(int(outcomes[x])))
-	total += int(outcomes[x])
-	#print("total value: " + str(total))
-
-average_win = total / iterations * 100
-print("\nTotal wins: " + str(total) + " out of " + str(iterations))
-print ("We won " + str(average_win) + "% of the time.")
+run_count = ask_for_iterations()
+switch = ask_for_switch()
+win_count = 0
+for i in range(run_count):
+	win_count += int(play_game(switch))
+print_statistics(win_count, run_count)
